dfa015a6594ef7fff30007fd9de453d3c44a951b
nickbaf <nickbaf@icloud.com>
2019-07-25 11:15:08 +0100

    bug fixing

---
+++ src/routes/RoutesAdmin.js
@@ -0,0 +1,62 @@
+// @material-ui/icons
+import Dashboard from '@material-ui/icons/Dashboard'
+import Person from '@material-ui/icons/Person'
+import Notifications from '@material-ui/icons/Notifications'
+// import Out from "@material-ui/icons/ArrowBack";
+import Setting from '@material-ui/icons/Settings'
+// core components/views for Admin layout
+import DashboardPage from 'views/Dashboard/Dashboard.jsx'
+import UserProfile from 'views/UserProfile/UserProfile.jsx'
+import TableList from 'views/TableList/TableList.jsx'
+import Typography from 'views/Typography/Typography.jsx'
+import NotificationsPage from 'views/Notifications/Notifications.jsx'
+
+// core components/views for RTL layout
+
+const dashboardRoutes = [
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    rtlName: 'لوحة القيادة',
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: '/admin'
+  },
+  {
+    path: '/table',
+    name: 'Triage List(Will be removed)',
+    rtlName: 'قائمة الجدول',
+    icon: 'content_paste',
+    component: TableList,
+    layout: '/admin'
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    rtlName: 'لوحة القيادة',
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: '/admin'
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    rtlName: 'لوحة القيادة',
+    icon: Setting,
+    component: DashboardPage,
+    layout: '/admin'
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    rtlName: 'لوحة القيادة',
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: '/admin'
+  }
+  
+
+
+]
+
+export default dashboardRoutes
---
+++ src/routes/RoutesTrust.js
@@ -0,0 +1,60 @@
+// @material-ui/icons
+import Dashboard from '@material-ui/icons/Dashboard'
+import Person from '@material-ui/icons/Person'
+import Notifications from '@material-ui/icons/Notifications'
+// import Out from "@material-ui/icons/ArrowBack";
+import Setting from '@material-ui/icons/Settings'
+// core components/views for Admin layout
+import TransitEnterexit from '@material-ui/icons/TransitEnterexit'
+import TrustPage from 'views/Dashboard/TrustDashboard.jsx'
+import UserProfile from 'views/UserProfile/UserProfile.jsx'
+import TableList from 'views/TableList/TableList.jsx'
+import Typography from 'views/Typography/Typography.jsx'
+import NotificationsPage from 'views/Notifications/Notifications.jsx'
+
+// core components/views for RTL layout
+
+const dashboardRoutes = [{
+  path: '/dashboard',
+  name: 'Dashboard',
+  rtlName: 'لوحة القيادة',
+  icon: Dashboard,
+  component: TrustPage,
+  layout: '/trust'
+},
+{
+  path: '/services',
+  name: 'IAPT Services',
+  rtlName: 'لوحة القيادة',
+  icon: Dashboard,
+  component: TrustPage,
+  layout: '/trust'
+},
+{
+  path: '/profile',
+  name: 'Profile',
+  rtlName: 'لوحة القيادة',
+  icon: Dashboard,
+  component: TrustPage,
+  layout: '/trust'
+},
+{
+  path: '/settings',
+  name: 'Settings',
+  rtlName: 'لوحة القيادة',
+  icon: Setting,
+  component: TrustPage,
+  layout: '/trust'
+},
+{
+  path: '/logout',
+  name: 'Logout',
+  rtlName: 'لوحة القيادة',
+  icon: Dashboard,
+  component: TrustPage,
+  layout: '/trust'
+}
+
+]
+
+export default dashboardRoutes
