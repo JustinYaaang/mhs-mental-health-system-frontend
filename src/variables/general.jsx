ac2a4a1bcefee7480ba0e75393e11622a2838521
Adebusola Adegbuyi <busseolafusca@gmail.com>
2019-07-29 14:42:58 +0100

    urls change

--- src/variables/general.jsx
+++ src/variables/general.jsx
@@ -18,8 +18,10 @@ var server = [
   'Sign contract for "What are conference organizers afraid of?"'
 ]
 
-var baseUrl = 'http://localhost:3000/api/v1'//http://mhsbackend.azurewebsites.net/api/v1/'
-var fetchQuestionnairesUrl = '/questionnaire_sJS'
+var baseUrl = 'http://178.128.34.125/api/v1'
+//'http://localhost:3000/api/v1'//http://mhsbackend.azurewebsites.net/api/v1/'
+var fetchQuestionnairesUrl = '/questionnaire'
+// '/questionnaire_sJS'
 var patientanswersUrl = '/patientanswers'
 var backendUrl = 'http://178.128.34.125/api/v1'
 var createUserAnswers = '/useranswers'
