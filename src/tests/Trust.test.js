01822fac2b057da25c85232cf7eed1196fa3f960
nickbaf <nickbaf@icloud.com>
2019-08-06 12:30:16 +0100

    Testing, testing.Everything seems to be in order.

--- src/tests/Trust.test.js
+++ src/tests/Trust.test.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import TrustDetails from '../layouts/Trusts/TrustDetails'
 import TrustList from '../layouts/Trusts/TrustList'
+import TrustCreateManager from '../layouts/Trusts/TrustCreateManager'
+import TrustCreateNew from '../layouts/Trusts/TrustCreateNew'
+import TrustUserDetails from '../layouts/Trusts/TrustUserDetails'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
@@ -20,3 +23,25 @@ describe('TrustList', () => {
        expect(component).toMatchSnapshot()
     })
   })
+
+  describe('Trust Create Manager', () => {
+   it('should render correctly', async () => {
+      const component = shallow(<TrustCreateManager />)
+ 
+      expect(component).toMatchSnapshot()
+   })
+ })
+ describe('Trust Create New', () => {
+   it('should render correctly', async () => {
+      const component = shallow(<TrustCreateNew />)
+ 
+      expect(component).toMatchSnapshot()
+   })
+ })
+ describe('Trust User Details', () => {
+   it('should render correctly', async () => {
+      const component = shallow(<TrustUserDetails />)
+ 
+      expect(component).toMatchSnapshot()
+   })
+ })
